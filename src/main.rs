0061ba36a416ad2318ab3278156b54a92a488ec2
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-10-30 20:10:53 +0100

    Implement very basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+use std::env;
 use std::process;
 use std::process::Command;
 
 fn main() {
+    let mut args = env::args();
+    args.next();
+
+    let search_for_class = args.next().unwrap();
+    let program_to_execute = args.next().unwrap();
+
+
     let output = Command::new("ratpoison").arg("-c").arg("windows %c,%n,%l,%s,%a,%t").output().ok().expect("Failed to retrieve list of windows from Ratpoison");
     let output = String::from_utf8_lossy(output.stdout.as_slice());
     for line in output.lines()
-        .filter(|l| l.starts_with("Firefox")) {
+        .filter(|l| l.starts_with(&search_for_class)) {
             let mut fields = line.split(',');
             fields.next();
             let window_number = fields.next().unwrap();
@@ -13,4 +21,6 @@ fn main() {
             Command::new("ratpoison").arg("-c").arg(rp_command).output().ok().expect("Failed to switch windows in Ratpoison");
             process::exit(0);
         }
+    Command::new(program_to_execute).spawn().expect("Failed to open program");
+    process::exit(0);
 }
