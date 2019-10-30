8d32f557ba512c9a9e1aec877e5c7c08022bfb5e
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-10-30 19:56:42 +0100

    Refactor: use filter

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,13 @@ use std::process::Command;
 fn main() {
     let output = Command::new("ratpoison").arg("-c").arg("windows %c,%n,%l,%s,%a,%t").output().ok().expect("Failed to retrieve list of windows from Ratpoison");
     let output = String::from_utf8_lossy(output.stdout.as_slice());
-    for line in output.lines() {
-        let mut vec = line.split(',');
-        let classname = vec.next();
-        match classname {
-            Some(classname) if classname == "Firefox" => {
-                println!("{}", classname);
-                let window_number = vec.next().unwrap();
-                let rp_command = format!("select {}", window_number);
-                Command::new("ratpoison").arg("-c").arg(rp_command).output().ok().expect("Failed to switch windows in Ratpoison");
-                process::exit(0);
-            }
-            Some(_) => (),
-            None => (),
+    for line in output.lines()
+        .filter(|l| l.starts_with("Firefox")) {
+            let mut fields = line.split(',');
+            fields.next();
+            let window_number = fields.next().unwrap();
+            let rp_command = format!("select {}", window_number);
+            Command::new("ratpoison").arg("-c").arg(rp_command).output().ok().expect("Failed to switch windows in Ratpoison");
+            process::exit(0);
         }
-    }
 }
