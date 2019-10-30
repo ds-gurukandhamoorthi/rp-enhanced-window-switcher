8c7697dcba94e45c23c2977bc7af7b8ec1116212
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-10-30 23:50:29 +0100

    Refactor: use nth, avoid exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::process;
 use std::process::Command;
 
 fn main() {
@@ -12,15 +11,18 @@ fn main() {
 
     let output = Command::new("ratpoison").arg("-c").arg("windows %c,%n,%l,%s,%a,%t").output().ok().expect("Failed to retrieve list of windows from Ratpoison");
     let output = String::from_utf8_lossy(output.stdout.as_slice());
-    for line in output.lines()
-        .filter(|l| l.starts_with(&search_for_class)) {
-            let mut fields = line.split(',');
+    let window_searched_for = output.lines()
+        .filter(|l| l.starts_with(&search_for_class))
+        .nth(0);
+
+    match window_searched_for {
+        Some(window) => {
+            let mut fields = window.split(',');
             fields.next();
             let window_number = fields.next().unwrap();
             let rp_command = format!("select {}", window_number);
             Command::new("ratpoison").arg("-c").arg(rp_command).output().ok().expect("Failed to switch windows in Ratpoison");
-            process::exit(0);
-        }
-    Command::new(program_to_execute).spawn().expect("Failed to open program");
-    process::exit(0);
+        },
+        None => {Command::new(program_to_execute).spawn().expect("Failed to open program");},
+    }
 }
