15cdb8bf91df0257904946f435200e9cb1172e5a
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-11-08 16:16:54 +0100

    Improve constructor of WinInfo, Add quickfix to solve when there is no windows

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::process::Command;
+use std::process;
 
 #[derive(Debug)]
 struct WinInfo {
@@ -13,15 +14,37 @@ struct WinInfo {
 }
 
 impl WinInfo {
-    pub fn new(info: &str) -> WinInfo {
+    pub fn new(info: &str) -> Result<WinInfo, &'static str> {
         let mut fields = info.split(',');
-        let classname = fields.next().unwrap().to_string();
-        let number: u32 = fields.next().unwrap().parse().expect("Failed to parse window number");
-        let last_access: u32 = fields.next().unwrap().parse().expect("Failed to parse last access");
-        let status: char = fields.next().unwrap().parse().unwrap();
-        let screen: u32 = fields.next().unwrap().parse().expect("Failed to parse screen number");
-        let application_name = fields.next().unwrap().to_string();
-        let title = fields.next().unwrap().to_string();
+        let classname = match fields.next() {
+            Some(s) => s.to_string(),
+            None => return Err("Unable to extract class name"),
+        };
+        let number: u32 = match fields.next().unwrap().parse() {
+            Ok(ui) => ui,
+            Err(_) => return Err("Unable to parse window number as integer"),
+        };
+        let last_access: u32 = match fields.next().unwrap().parse() {
+            Ok(ui) => ui,
+            Err(_) => return Err("Unable to last access as integer"),
+        };
+        let status: char = match fields.next().unwrap().parse() {
+            Ok(c) => c,
+            Err(_) => return Err("Unable to status as character"),
+        };
+        let screen: u32 = match fields.next().unwrap().parse() {
+            Ok(ui) => ui,
+            Err(_) => return Err("Unable to screen as integer"),
+        };
+        let application_name = match fields.next() {
+            Some(s) => s.to_string(),
+            None => return Err("Unable to extract application name"),
+        };
+        let title = match fields.next() {
+            Some(s) => s.to_string(),
+            None => return Err("Unable to extract application name"),
+        };
+        Ok(
         WinInfo{
             classname,
             application_name,
@@ -30,7 +53,7 @@ impl WinInfo {
             status,
             screen,
             title,
-        }
+        })
     }
 }
 
@@ -42,14 +65,24 @@ fn main() {
     let program_to_execute = args.next().unwrap();
 
 
-    let output = Command::new("ratpoison").arg("-c").arg("windows %c,%n,%l,%s,%S,%a,%t").output().expect("Failed to retrieve list of windows from Ratpoison");
+    let output = Command::new("ratpoison").arg("-c").arg("windows %c,%n,%l,%s,%S,%a,%t").output();
+    let output = output.unwrap();
     let output = String::from_utf8_lossy(output.stdout.as_slice());
+
+    if output.clone().starts_with("No"){ //Quickfix to solve the opening program when there is no ratpoison windows
+        Command::new(&program_to_execute).spawn().expect("Failed to open program");
+        process::exit(0);
+    }
+
     let windows_with_same_class = output.lines()
         .map(WinInfo::new)
+        .filter_map(Result::ok)
         .filter(|w| w.classname.starts_with(&search_for_class));
+
 
     let window_searched_for = windows_with_same_class
         .max_by_key(|w| w.last_access);
+
 
     match window_searched_for {
         Some(window) => {
